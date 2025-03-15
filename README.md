3efbbbf552a792ed83c01639789091d7ce23be43
aaronlamz <aaronlamz2022@gmail.com>
2025-03-15 11:35:29 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sms-activate-platform
 接码短信平台
+
+# 启动
+> nodejs版本: v14.21.3
+```shell
+yarn install
+yarn serve
+```
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'hash', // 使用 hash 模式，类似 k20.cc 的路由结构
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    { path: '/login', name: 'login', component: Login },
+    { path: '/register', name: 'register', component: Register }
+  ]
+})
