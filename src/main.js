eb3cb46861b80ae3accf661d188ef94898abefd9
aaronlamz <aaronlamz2022@gmail.com>
2025-03-26 10:09:22 +0800

    stylefix confirm dialog

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/styles/reset.css'
+import '@/assets/styles/global.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, {
